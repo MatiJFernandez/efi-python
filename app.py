6ce5c5faf87c5b3be5632c6a1093741bcdc51342
matias <mj.fernandez@itecriocuarto.org.ar>
2024-07-12 11:30:41 -0300

    importa y configura sqlalchemy y migrate en app, crea models

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate   
+
+
 
 app = Flask(__name__)
+#config Alchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/efi' #aca se establece la cadena de conexion a la DB
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route("/")
 def home():
